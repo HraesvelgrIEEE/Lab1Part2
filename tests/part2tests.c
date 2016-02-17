6ab6b305a60a767fdfaf6686d0f595f8ebae63e9
David <davidhung@email.arizona.edu>
2016-02-17 12:49:01 -0700

    Initial commit

---
+++ tests/part2tests.c
@@ -0,0 +1,16 @@
+/* part2tests.c
+ * Author: dhung
+ * Lab 1
+ */
+
+#include <xc.h>
+#include "part2tests.h"
+#include "timer.h"
+
+bool delayUsTest() {
+	
+}
+
+bool LCDWriteTest() {
+
+}
---
+++ tests/part2tests.h
@@ -0,0 +1,12 @@
+/* part2tests.h
+ * Author: dhung
+ */
+
+#ifndef LAB1_PART2_TESTS
+#define LAB1_PART2_TESTS
+
+bool delayUsTest();
+bool LCDWriteTest();
+
+
+#endif
